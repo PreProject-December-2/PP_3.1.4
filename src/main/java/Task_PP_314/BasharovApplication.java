cac32285a987a4af0438980b6717b300e3a38518
Anatoliy Basharov <rexonka3@gmail.com>
2024-04-09 15:52:07 +0300

    Initial commit

---
+++ src/main/java/Task_PP_314/BasharovApplication.java
@@ -0,0 +1,13 @@
+package Task_PP_314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasharovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasharovApplication.class, args);
+	}
+
+}
